f8c89e09b9f2874b4e96ad8100fcb5028e801a6a
Ian Alderman <na>
2017-06-16 14:06:46 +0100

    Updated Ansible HOME Vars

--- scripts/provsvr.sh
+++ scripts/provsvr.sh
@@ -8,4 +8,6 @@ apt-get update -y
 apt-get install ansible -y
 
 mkdir /etc/ansible/conf
+ANSIBLE_LOCAL_TEMP=$HOME/.ansible/tmp
+ANSIBLE_REMOTE_TEMP=$HOME/.ansible/tmp
 ansible-pull -d /etc/ansible/init -U https://github.com/ianalderman/holansiblepull.git
